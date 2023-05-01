994e0d00fa31034f002aff5c8b8aca52c5bf24ef
imadteryaki <115640742+imadteryaki@users.noreply.github.com>
2023-05-01 20:41:54 +0200

    Update the project

--- index.js
+++ index.js
@@ -3,6 +3,9 @@ console.clear();
 // readline module
 const readline = require('readline');
 
+// colors 
+const colors = require('colors');
+
 class TicTacToe {
   constructor() {
     // Variables
@@ -14,6 +17,8 @@ class TicTacToe {
 
     // readline declare
     this.rl = readline.createInterface(process.stdin, process.stdout);
+
+    
   }
 
   // replace empty places on layout
@@ -31,7 +36,7 @@ class TicTacToe {
         ----+------+----
         ${this.replaceEmptyPlaces(this.ticTacToe[6])}   |  ${this.replaceEmptyPlaces(this.ticTacToe[7])}   |  ${this.replaceEmptyPlaces(this.ticTacToe[8])}`;
 
-    console.log(this.gameLayout);
+    console.log(colors.yellow(this.gameLayout));
   }
 
   // getter
@@ -51,15 +56,17 @@ class TicTacToe {
   //  Check the position
   checkPosition(position) {
     // Check if position is existed
-    if (position > 9 || position < 1) {
-      console.log(`This position isn't existed, please choose a position between 1 and 9`);
+    if (position > 9 || position < 1 || Number.isNaN(position)) {
+      console.log(colors.bgRed(`\nThis position isn't existed, please choose a position between 1 and 9`));
       this.currentPlayer = !this.currentPlayer;
+      return;
     }
     // Check if position is already taken
     else if (this.ticTacToe[position - 1] !== undefined) {
       // empty position = undefined (see line 22)
-      console.log(`Position number ${position} is already taken, please choose other position between 1 and 9`);
+      console.log(colors.bgRed(`\nPosition number ${position} is already taken, please choose other position between 1 and 9`));
       this.currentPlayer = !this.currentPlayer;
+      return;
     } else {
       // Registration the player's char
       this.ticTacToe[position - 1] = this.getChar();
@@ -93,9 +100,12 @@ class TicTacToe {
             (this.ticTacToe[6] && this.ticTacToe[7] && this.ticTacToe[8] && 
             (Array.from(checkSet.add(this.ticTacToe[6]).add(this.ticTacToe[7]).add(this.ticTacToe[8])).length === 1 ))){
 
-            console.log(`\nPlayer ${this.getPlayerFromChar(Array.from(checkSet)[0])} Wins!!`);
+              if(this.currentPlayer)
+              console.log(colors.blue(`\nPlayer ${this.getPlayerFromChar(Array.from(checkSet)[0])} Wins!!`));
+              else
+              console.log(colors.red(`\nPlayer ${this.getPlayerFromChar(Array.from(checkSet)[0])} Wins!!`));
            
-            this.gameEnd();
+            this.resetGame();
         }
        
         // possible vertical alignments 
@@ -110,7 +120,10 @@ class TicTacToe {
            (this.ticTacToe[2] && this.ticTacToe[5] && this.ticTacToe[8] && 
            (Array.from(checkSet.add(this.ticTacToe[2]).add(this.ticTacToe[5]).add(this.ticTacToe[8])).length === 1 ))){
 
-           console.log(`\nPlayer ${this.getPlayerFromChar(Array.from(checkSet)[0])} Wins!!`);
+            if(this.currentPlayer)
+            console.log(colors.blue(`\nPlayer ${this.getPlayerFromChar(Array.from(checkSet)[0])} Wins!!`));
+            else
+            console.log(colors.red(`\nPlayer ${this.getPlayerFromChar(Array.from(checkSet)[0])} Wins!!`));
            
            this.gameEnd();
        }
@@ -124,8 +137,11 @@ class TicTacToe {
           (this.ticTacToe[2] && this.ticTacToe[4] && this.ticTacToe[6] && 
            (Array.from(checkSet.add(this.ticTacToe[2]).add(this.ticTacToe[4]).add(this.ticTacToe[6])).length === 1 ))){
 
-           console.log(`\nPlayer ${this.getPlayerFromChar(Array.from(checkSet)[0])} Wins!!`);
-           
+            if(this.currentPlayer)
+           console.log(colors.blue(`\nPlayer ${this.getPlayerFromChar(Array.from(checkSet)[0])} Wins!!`));
+           else
+           console.log(colors.red(`\nPlayer ${this.getPlayerFromChar(Array.from(checkSet)[0])} Wins!!`));
+
            this.gameEnd();
        }
 
@@ -133,13 +149,29 @@ class TicTacToe {
         checkSet.clear();
          if (this.moveHistory.length === 9){
 
-            console.log(`\nGame ended in a Draw !!`)
+            console.log(colors.yellow(`\nGame ended in a Draw !!`));
             
-            this.gameEnd()
-        } 
-        
-    }
+            this.gameEnd();
+        }
 
+       if (this.gameEnded) {
+        if (this.gameEnded) {
+          this.rl.question (colors.green('\nPress R + Enter to restart the game or press only Enter to end : '),(input) => {
+            if ( input.toLowerCase() == 'r') {
+              console.clear();
+              this.resetGame();
+              this.rl.prompt();
+              this.run();
+              return;
+            }
+            else {
+              console.log(colors.green('\nThank you for playing this game , See you later !'));
+              this.rl.close();
+            }})
+        }
+      
+       } 
+    }
 }
 
 switchPlayer() {
@@ -149,40 +181,132 @@ switchPlayer() {
     
     if(!this.gameEnded){
         this.currentPlayer = arguments[0] ? this.currentPlayer : !this.currentPlayer;
-        console.log(`\nPlayer ${this.getPlayer(this.currentPlayer)}, Your move! (Position [1 - 9]):`)
+        if (this.currentPlayer){
+          this.rl.question(colors.blue(`\nPlayer ${this.getPlayer(this.currentPlayer)}, Your move! (Position [1 - 9]): `),(input) => {
+            if(this.ticTacToe.length < 10 ){
+              this.checkPosition(parseInt(input));
+              this.switchPlayer();
+             }
+             else {
+               this.gameProcess();
+             }
+           })
+        }
+        else 
+        {
+          this.rl.question(colors.red(`\nPlayer ${this.getPlayer(this.currentPlayer)}, Your move! (Position [1 - 9]): `),(input) => {
+            if(this.ticTacToe.length < 10 ){
+              this.checkPosition(parseInt(input));
+              this.switchPlayer();
+             }
+             else {
+               this.gameProcess();
+             }
+           })
+        }
+
+        
     }
-    
 }
 
 run() {
+   console.log(colors.red('Welcome to my Tic Tac Toe\n'));
+   setTimeout(()=>{
+    console.log(colors.green(`This game has one rule which is any player get three character in a row wins !!\n`));
+   },1000);
+   
+   setTimeout(()=>{
+    console.log(colors.green(`to play this game we need tow players\n`));
+   },2000);
 
-  console.log('Welcome to my Tic Tac Toe');
-   this.layout() 
-   console.log(`\nPlayer ${this.getPlayer(this.currentPlayer)}, Your move! (Position [1 - 9]):`)
+   setTimeout(()=>{
+    console.log(colors.cyan(`Now you will see the game layout\n`));
+   },3000);
 
-   // listen to inputs
-   this.rl.on('line',(input) => {
-    if(this.ticTacToe.length < 10 ){
+   setTimeout(()=>{
+    this.layout();
+    console.log(`\n`)
+   },4000);
+   
+   setTimeout(()=> {
+    console.log(colors.yellow(`Let's start the game !!\n`));
+   },5000)
+   setTimeout(()=> {
+    console.log(colors.red(`Note: to delete the last move press Ctrl + B\n`));
+   },6000)
+   
+   
+   // remove last play turn
+   readline.emitKeypressEvents(process.stdin);
+   process.stdin.setRawMode(true);
+   process.stdin.on('keypress', async (str, key) => {
+      if (key.name =='b' && key.ctrl) {
+        this.deleteLastMove()
+       }
+     })
+   
+    
+  // listen to inputs
+  setTimeout(() => {
+    this.rl.question(colors.blue(`Player ${this.getPlayer(this.currentPlayer)}, Your move! (Position [1 - 9]): `),(input) => {
+      if(this.ticTacToe.length < 10 ){
         this.checkPosition(parseInt(input));
         this.switchPlayer();
-    }
-    else {
-        console.log(`Game Ended`);
-        this.gameProcess();
-      }
-    })
-  }
-  
+       }
+       else {
+         this.gameProcess();
+       }
+     })
+   },6000   
+  )
+   
+  }  
 gameEnd() {
     
   // Set gameEnded to true to stop the game in the switchPlayer() method.
   this.gameEnded = true;
   
-  console.log(`${this.moveHistory.length} moves needed\nMoves history ...`);
+  console.log(colors.magenta(`\n${this.moveHistory.length} moves needed\n\nMoves history ...\n`));
   console.log(this.moveHistory);
+}
+
+deleteLastMove(){
+  // delete character
+  if(this.moveHistory.length > 1){
+    var pos = this.moveHistory[(this.moveHistory.length - 1)].position
+    this.ticTacToe[pos] = ' '
+    // unregister move 
+    this.moveHistory.pop();
+    // continue play 
+    this.switchPlayer(true)
+    console.log(colors.bgGreen('\nLast move has been deleted !!'))
+    
+  } else {
+    console.log(colors.bgYellow("\nCan't delete any more moves!!"));
+    // continue play
+    this.switchPlayer(true); // don't change player with the next move
+  }
 
 }
+
+resetGame() {
+
+    this.ticTacToe = new Array(9);
+    this.currentPlayer = true;
+    this.moveHistory = [];
+    this.gameEnded = false;
+
+    // close the current interface 
+    this.rl.close();
+
+    // create a new interface 
+    this.rl = readline.createInterface(process.stdin, process.stdout);
+    console.clear();
+
 }
+
+}
+
 const game = new TicTacToe();
 game.run();
 
